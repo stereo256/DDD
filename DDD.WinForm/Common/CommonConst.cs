9c88354cd1fa76ef2682e7733235e3d10be92d5d
stereo256 <skyfull000@gmail.com>
2020-10-18 13:19:39 +0900

    #08_BADコード改善

---
+++ DDD.WinForm/Common/CommonConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WinForm.Common
+{
+    public static class CommonConst
+    {
+        public const string TemperatureUnitName = "℃";
+        public const int TemperatureDecimalPoint = 2;
+        public const string ConnectionString = @"Data Source = C:\Users\skyfu\OneDrive\デスクトップ\DDD.db;Version=3";
+
+    }
+}
